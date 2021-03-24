783b5eb37aaa827117ecf5ed07d321c0435f273c
Mohamed-Saeed1 <73955495+Mohamed-Saeed1@users.noreply.github.com>
2021-03-24 15:57:51 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 let list = document.getElementById('list');
 let containerLength = document.getElementsByClassName('landing__container').length;
 
+
+
 for (var i = 1; i <= containerLength; i++){
 	
 //Giving section its name, id and class to add it to the ul.
@@ -9,7 +11,7 @@ for (var i = 1; i <= containerLength; i++){
 	let sectext = secval.textContent;
 	let newitem = document.createElement("li");
 	let newtext = document.createTextNode(sectext);
-	let secitem = "sample-nav" + i;
+	let secitem = "section" + i + "x";
 	newitem.setAttribute("id", secitem);
 	newitem.setAttribute("class", "nav-list-item");
 	newitem.appendChild(newtext);
@@ -23,9 +25,43 @@ for (var i = 1; i <= containerLength; i++){
 
 	listtarget.addEventListener("click", function(){
 		sectarget.scrollIntoView({
-			behavior: "smooth"
+			behavior: "smooth" //smooth scroll property to give scroll effect to user
 		})
 	});
 
 }
+//getting section in view
+function InView() {
+  let isInViewport = function(x) {
+    let bound = x.getBoundingClientRect();
+    return (
+      bound.top <= 50 && bound.top >= -500 &&
+      bound.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      bound.right <=
+        (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
+  for (i = 1; i < containerLength + 1; i++) {
+    let sectionInView = document.getElementById("section"+i);
+    let sectab = document.getElementById("section"+i+"x");
+
+    window.addEventListener(
+      "scroll",
+      //giving active section active class and it corresponding item in nav list
+      function(event) {
+        if (isInViewport(sectionInView)) {
+          	sectionInView.classList.add("your-active-class");
+			sectab.classList.add('activeclass');
+        } else {
+         	sectionInView.classList.remove("your-active-class");
+			sectab.classList.remove('activeclass');
+        }
+      },
+      false
+    );
+  }
+}
+InView();
 
